de8bee66d751b899225ac803791f1b0fabba3b05
unboundhealing <unboundhealingministries@gmail.com>
2026-06-21 19:30:19 -0400

    Add plugin registry for page enhancements

---
+++ scripts/plugin-registry.py
@@ -0,0 +1,11 @@
+from enhance_pages import (
+    plugin_related_content,
+    plugin_tracking,
+    plugin_future_magic,
+)
+
+PLUGIN_REGISTRY = {
+    "related_content": plugin_related_content,
+    "tracking": plugin_tracking,
+    "future_magic": plugin_future_magic,
+}
