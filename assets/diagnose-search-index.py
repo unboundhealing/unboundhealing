c16fcb99bf3f46ffaa007dc95331fcac299bb61b
unboundhealing <unboundhealingministries@gmail.com>
2026-06-28 19:01:11 -0400

    Add script to diagnose search index from JSON

---
+++ assets/diagnose-search-index.py
@@ -0,0 +1,10 @@
+import json
+
+with open("search-index.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+sample = next(iter(data.values()))
+
+print("SAMPLE ENTRY:")
+for k, v in sample.items():
+    print(k, "=>", type(v), ":", str(v)[:60])
