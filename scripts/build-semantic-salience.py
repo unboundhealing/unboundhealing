759abea014ebd603e663c5ba8ffcd3682964aa98
unboundhealing <unboundhealingministries@gmail.com>
2026-06-30 23:47:23 -0400

    Add additional stopwords to the list

--- scripts/build-semantic-salience.py
+++ scripts/build-semantic-salience.py
@@ -17,7 +17,7 @@ STOPWORDS = {
     "https", "http", "www", "com", "org", "html", "indexhtml", "index",
     "a", "an", "the", "and", "or", "of", "to", "in", "on", "at", "for", "with", "from", 
     "is", "are", "was", "were", "be", "been", "that", "these", "those", "it", "its", "it's",
-    "as", "by", "into", "though", "through"
+    "as", "by", "into", "though", "through", "i", "me", "my", "myself"
 }
 
 # =========================================================
