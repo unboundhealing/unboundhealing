03aebeaac8affcad8c074d70ae00a1bfac7e6dbe
unboundhealing <unboundhealingministries@gmail.com>
2026-07-05 14:50:19 -0400

    Remove debug echo from generate-site.sh
    Removed debug print statement from generate-site.sh

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -68,7 +68,5 @@ git push
 # FINAL STATE DECLARATION
 # ---------------------------------------------------------
 
-echo "📜 DEBUG: printing embedded python script"
-
 echo "🧭 FINAL STATE: semantic-salience is the ONLY truth layer"
 echo "🧱 legacy semantic graph systems are archived / disabled"
